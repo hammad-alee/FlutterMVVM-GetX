e0f13ef18a3b7250bcbed8c8cd0e801e4bbd48fd
hammad-alee <hammaddali143@gmail.com>
2023-05-26 18:33:01 +0500

    init

--- lib/routes/app_route.dart
+++ lib/routes/app_route.dart
@@ -1,14 +1,18 @@
 import 'package:get/get.dart';
+import 'package:mvvm_getx/routes/app_route_name.dart';
+import 'package:mvvm_getx/view/splash_view.dart';
+
+import '../view_model/splash_view_model/splash_bin.dart';
 
 class AppRoutes {
   static List<GetPage> pages = [
-    // GetPage(
-    //   name: initialRoute,
-    //   page: () => const SplashScr(),
-    //   bindings: [
-    //     SplashBin(),
-    //   ],
-    // ),
+    GetPage(
+      name: AppRouteName.initialRoute,
+      page: () => const SplashView(),
+      bindings: [
+        SplashBin(),
+      ],
+    ),
     // GetPage(
     //   name: login,
     //   page: () => const LoginScr(),
